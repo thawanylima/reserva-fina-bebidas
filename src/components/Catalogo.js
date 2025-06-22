1cda74a4df70ee84af8e0d5edff2920613626a6c
Thawany <thawanylimaa@outlook.com>
2025-06-21 21:32:14 -0300

    update codigo componente catalogo

--- src/components/Catalogo.js
+++ src/components/Catalogo.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const Catalogo = () =>(
+const Catalogo = ( {bebidas} ) =>{
+    return(
     <main className="principal">
-        <h2>Catalogo de Bebidas</h2>
-        <ul>
-            <li>
-                <a href="#">Bebida 1</a>
-            </li>
-            <li>
-                <a href="#">Bebida 2</a>
-            </li>
-        </ul>
+        <h2>Catalogo de Vinhos</h2>
+        <ol>
+            {bebidas.filter(bebida => bebida.tipo == "Vinho").map(bebida => (
+                <li><Link to={'/bebida/${bebida.slug}'}>{bebida.nome}</Link></li>))}
+        </ol>
+        <h2>Catálogo Espumantes</h2>
+        <ol>
+            {bebidas.filter(bebida => bebida.tipo == "Espumante").map(bebida => (
+                <li><Link to={'/bebida/${bebida.slug}'}>{bebida.nome}</Link></li>))}
+        </ol>
+        <h2>Catálogo de Whiskys</h2>
+        <ol>
+             {bebidas.filter(bebida => bebida.tipo == "Whisky").map(bebida => (
+                <li><Link to={'/bebida/${bebida.slug}'}>{bebida.nome}</Link></li>))}
+        </ol>
     </main>
-)
+    );
+};
 export default Catalogo;
